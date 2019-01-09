46c4402a78ca19f37190fc2c47b2cd8d91506ef4
jamelouis <jamelouis2015@gmail.com>
2019-01-09 20:02:54 +0800

    conform textfielddelegate

--- EditBox-swift/EditBox-swift/ViewController.swift
+++ EditBox-swift/EditBox-swift/ViewController.swift
@@ -18,3 +18,10 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
