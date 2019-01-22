9c15c96722a231dde5d270f4e45f0fd676671b46
jamelouis <jamelouis2015@gmail.com>
2019-01-22 14:20:00 +0800

    create basic opengles ios project
    1. create a ios project by selecting a single view app template
    2. remove view controller scene from main.storyboard
    3. add glkit controller scene to main.storyboard and set it as initial view controller
    4. setup gl context and clear color to purple

---
+++ OpenGLES/OpenGLES/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  OpenGLES
+//
+//  Created by jamelouis on 2019/1/22.
+//  Copyright © 2019 jamelouis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
---
+++ OpenGLES/OpenGLES/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  OpenGLES
+//
+//  Created by jamelouis on 2019/1/22.
+//  Copyright © 2019 jamelouis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    GLKView *view = (GLKView*)self.view;
+    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    
+    view.drawableColorFormat = GLKViewDrawableColorFormatRGBA8888;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    view.drawableStencilFormat = GLKViewDrawableStencilFormat8;
+    
+    view.drawableMultisample = GLKViewDrawableMultisample4X;
+}
+
+-(void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+@end
