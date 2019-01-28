aa5ba4feb2f852b6da9de0fabc82745a15b2c148
jamelouis <jamelouis2015@gmail.com>
2019-01-28 15:52:18 +0800

    [table view] show 10 fake element

--- table-view/table-view/ViewController.swift
+++ table-view/table-view/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let label = item.viewWithTag(1000) as! UILabel
+        label.text = String(indexPath.row)
 
+        return item
+    }
+    
 }
 
