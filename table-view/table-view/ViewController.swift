9989f3f6e89ecd167199b1473de4c848b776ce1c
jamelouis <jamelouis2015@gmail.com>
2019-01-28 15:22:44 +0800

    [table view] a basic setup

---
+++ table-view/table-view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  table-view
+//
+//  Created by jamelouis on 2019/1/28.
+//  Copyright © 2019 jamelouis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
